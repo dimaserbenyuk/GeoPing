63401b9a6068036efbacb20cb3ffcffa6e6a0b8e
DimaSerbenyuk <dserbenyukgood@gmail.com>
2019-06-29 17:44:51 +0300

    add

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ require("dotenv").config();
 
 const typeDefs = require("./typeDefs");
 const resolvers = require("./resolvers");
-
 mongoose
   .connect(process.env.MONGO_URI, { useNewUrlParser: true })
   .then(() => console.log("DB connected!"))
