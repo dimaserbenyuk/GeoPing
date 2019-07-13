8b1a6ecf2c78798b1e4547e9e558c51872b328a2
DimaSerbenyuk <dserbenyukgood@gmail.com>
2019-07-13 14:38:42 +0300

    Add Map and PinIcon
    Pin for User's Current Position and Draft Pin

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,9 +1,12 @@
-import React, { useState } from "react";
-import ReactMapGL, { NavigationControl } from 'react-map-gl'
+import React, { useState, useEffect, useContext } from "react";
+import ReactMapGL, { NavigationControl, Marker } from 'react-map-gl'
 import { withStyles } from "@material-ui/core/styles";
 // import Button from "@material-ui/core/Button";
 // import Typography from "@material-ui/core/Typography";
 // import DeleteIcon from "@material-ui/icons/DeleteTwoTone";
+
+import PinIcon from './PinIcon'
+import Context from '../context'
 
 const INITIAL_VIEWPORT = {
   latitude: 37.7577,
@@ -12,7 +15,36 @@ const INITIAL_VIEWPORT = {
 }
 
 const Map = ({ classes }) => {
+  const { state, dispatch } = useContext(Context)
   const [viewport, setViewport] = useState(INITIAL_VIEWPORT)
+  const [userPosition, setUserPosition] = useState(null)
+  useEffect(() => {
+    getUserPosition()
+  }, [])
+
+  //latitude, longitude широта Долгота
+  const getUserPosition =()=> {
+    if("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(position =>{
+        const { latitude, longitude } = position.coords
+        setViewport({...viewport, latitude, longitude})
+        setUserPosition({latitude, longitude})
+      })
+    }
+  }
+
+  const handleMapClick = ({lngLat, leftButton}) => {
+    if (!leftButton) return
+    if (!state.draft) {
+      dispatch({type: "CREATE_DRAFT"})
+    }
+    const[longitude, latitude] = lngLat
+    dispatch({
+      type: "UPDATE_DRAFT_LOCATION",
+      payload: { longitude, latitude }
+    })
+  };
+
   return (
   <div className={classes.root}>
     <ReactMapGL
@@ -22,18 +54,42 @@ const Map = ({ classes }) => {
       mapboxApiAccessToken="pk.eyJ1IjoiZGltYXNlcmJlbnl1ayIsImEiOiJjankwbHp2MHUwMXYwM25vMTg4Y2d5aTk4In0.aLzhYd_0w-uMpWzuJN7Jag"
       onViewportChange={newViewport => setViewport
       (newViewport)}
+      onClick={handleMapClick}  
+
       {...viewport}
     >
-      {/**Navigation Control */}
+      {/* Navigation Control */}
       <div className={classes.navigationControl}>
-        <NavigationControl
+          <NavigationControl
             onViewportChange={newViewport => setViewport(newViewport)}
-        />
+          />
+        </div>
 
-      </div>
+        {/* Pin for User's Current Position */}
+        {userPosition && (
+          <Marker
+            latitude={userPosition.latitude}
+            longitude={userPosition.longitude}
+            offsetLeft={-19}
+            offsetTop={-37}
+          >
+            <PinIcon size={40} color="red" />
+          </Marker>
+        )}
+
+        {/* Draft Pin */}
+        {state.draft && (
+          <Marker
+            latitude={state.draft.latitude}
+            longitude={state.draft.longitude}
+            offsetLeft={-19}
+            offsetTop={-37}
+          >
+            <PinIcon size={40} color="hotpink" />
+          </Marker>
+        )}
 
     </ReactMapGL>
-
   </div>);
   
 };
--- client/src/reducer.js
+++ client/src/reducer.js
@@ -1,22 +1,38 @@
-export default function reducer(state,{type,payload}){
-    switch(type) {
-        case "LOGIN_USER":
-            return {
-                ...state,
-                currentUser: payload
-            };
-        case "IS_LOGGED_IN":
-            return {
-                ...state,
-                isAuth: payload
-            }    
-        case "SIGNOUT_USER":
-            return {
-                ...state,
-                isAuth: false
-            }    
-            default:
-                return state;
-        }
+export default function reducer(state, { type, payload }) {
+    switch (type) {
+      case "LOGIN_USER":
+        return {
+          ...state,
+          currentUser: payload
+        };
+      case "IS_LOGGED_IN":
+        return {
+          ...state,
+          isAuth: payload
+        };
+      case "SIGNOUT_USER":
+        return {
+          ...state,
+          isAuth: false,
+          currentUser: null
+        };
+      case "CREATE_DRAFT":
+        return {
+          ...state,
+          currentPin: null,
+          draft: {
+            latitude: 0,
+            longitude: 0
+          }
+        };
+       case "UPDATE_DRAFT_LOCATION":
+           return {
+               ...state,
+               draft: payload
+           }; 
+       default:
+        return state;
+        
         
     }
+}
