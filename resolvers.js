79e18c42e9bc4d670c0677a940bd1f2e1775f54e
DimaSerbenyuk <dserbenyukgood@gmail.com>
2019-09-01 21:43:10 +0300

    add mutations and client

--- resolvers.js
+++ resolvers.js
@@ -1,11 +1,7 @@
-const { AuthenticatedError } = require('apollo-server')
+const { AuthenticationError} = require("apollo-server");
+const Pin = require("./models/Pin");
 
-const user = {
-    _id: "1",
-    name: "Dima",
-    email: "dserbenyukgood@gmail.com",
-    picture: "https://cloudinary.com/asdf"
-};
+
 
 const authenticated = next => (root, args, ctx, info) => {
     if (!ctx.currentUser) {
@@ -16,6 +12,17 @@ const authenticated = next => (root, args, ctx, info) => {
 
 module.exports = {
   Query: {
-    me: authenticated((root, args, ctx) => ctx.currentUser)
+    me: authenticated((root, args, ctx) =>
+    ctx.currentUser)
+    },
+    Mutation: {
+      createPin: authenticated(async (root, args, ctx) => {
+        const newPin = await new Pin({
+          ...args.input,
+          author: ctx.currentUser._id
+        }).save();
+        const pinAdded = await Pin.populate(newPin, "author");
+        return pinAdded;
+      })
     }
-}
+ }
